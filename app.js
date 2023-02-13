99fc5bc3a3c14e870c727a687356a72d8927fffe
DrewMax <andrew.reviresco@gmail.com>
2023-02-13 19:52:40 +0100

    page template

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+const fs = require("fs");
+
+const generatePage = require("./page-template")
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs)
 
-const printProfileData = profileDataArr => {
+const [name, github] = profileDataArgs
+
+const printProfileData = (profileDataArr) => {
     for (let i = 0; i < profileDataArr; i += 1) {
         console.log(profileDataArr[i])
+        console.log("============")
+
+
+profileDataArr.forEach(profileItem => console.log(profileItem))
     }
 };
 
 printProfileData(profileDataArgs)
 
-console.log("============")
 
-profileDataArr.forEach(profileItem => console.log(profileItem))
+
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!")
+});
